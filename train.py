234a4aa1bc9201a7f7e8c372557e0f1faf9d0d84
Vishal R <mrvishal2k2@gmail.com>
2023-11-19 20:31:54 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,53 +1,76 @@
+# https://chat.openai.com/share/ae0f6fb0-6797-471c-91fd-f541c3f4f803
+# incomplete version
+# train_chatbot.py
+
+import nltk
+from nltk.stem import WordNetLemmatizer
 import numpy as np
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+import json
 
-# Load the customer chat conversation dataset
-df = pd.read_csv('customer_chat_conversations.csv')
+# Load intents file
+with open("data/intents.json") as file:
+    data = json.load(file)
 
-# Split the dataset into features and labels
-X = df['message_text']
-y = df['intent']
+# Extract data for training
+intents = data["intents"]
+patterns = []
+responses = []
+tags = []
 
-# Create a TF-IDF vectorizer
-vectorizer = TfidfVectorizer()
+for intent in intents:
+    for pattern in intent["patterns"]:
+        patterns.append(pattern.lower())
+        tags.append(intent["tag"])
+    responses.extend(intent["responses"])
 
-# Transform the features into TF-IDF vectors
-X_tfidf = vectorizer.fit_transform(X)
+# Tokenize words
+lemmatizer = WordNetLemmatizer()
+words = [lemmatizer.lemmatize(word) for word in patterns if word.isalnum()]
+words = sorted(set(words))
 
-# Create a logistic regression classifier
-clf = LogisticRegression()
+# Create training data
+training_data = []
+output_empty = [0] * len(tags)
 
-# Train the classifier
-clf.fit(X_tfidf, y)
+for idx, pattern in enumerate(patterns):
+    bag = []
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern.split() if word.isalnum()]
+    
+    for word in words:
+        bag.append(1) if word in pattern_words else bag.append(0)
 
-# Create a function to predict the intent of a new customer message
-def predict_intent(message):
-  """Predicts the intent of a new customer message.
+    output_row = list(output_empty)
+    output_row[tags.index(tags[idx])] = 1
 
-  Args:
-    message: A string containing the customer message.
+    training_data.append([bag, output_row])
 
-  Returns:
-    A string containing the predicted intent.
-  """
+# Shuffle the training data
+np.random.shuffle(training_data)
+training_data = np.array(training_data)
 
-  # Transform the message into a TF-IDF vector
-  message_tfidf = vectorizer.transform([message])
+# Split data into input and output
+train_x = list(training_data[:, 0])
+train_y = list(training_data[:, 1])
 
-  # Make a prediction
-  prediction = clf.predict(message_tfidf)
+# Build the model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation="softmax"))
 
-  # Return the predicted intent
-  return prediction[0]
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
-# Example usage:
+# Train the model
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 
-message = "I'm having trouble logging into my account."
+# Save the trained model
+model.save("models/chatbot_model.pkl", model_format="h5")
 
-# Predict the intent of the message
-intent = predict_intent(message)
-
-# Print the predicted intent
-print(intent)
+print("Chatbot model trained successfully.")
