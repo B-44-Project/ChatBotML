4a755cf4e82167c7ad2208c64b493e30588d6b46
Darshan Gowda G <darshangowdag.2002@gmail.com>
2023-11-25 09:49:32 +0530

    Responsive

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -229,3 +229,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+/*
+var div = document.getElementById('sidebar');
+var display = 0;
+
+function hideshow()
+{
+    if(display == 1){
+        div.style.display = 'block';
+        display = 0;
+    }else{
+        div.style.display = 'none';
+        display = 1;
+    }
+}
+*/
